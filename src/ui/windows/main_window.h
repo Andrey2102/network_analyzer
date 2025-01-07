64112971c2453846b67d38c163e27588638266f7
Vlad <vlad21452001@gmail.com>
2025-01-07 22:03:48 +0200

    Add basic ui

--- src/ui/windows/main_window.h
+++ src/ui/windows/main_window.h
@@ -5,6 +5,10 @@
 #include <QMenuBar>
 #include <QStatusBar>
 #include <QToolBar>
+#include <QVBoxLayout>
+#include <QTextEdit>
+#include <QPushButton>
+#include <QLabel>
 
 class MainWindow : public QMainWindow
 {
@@ -19,12 +23,34 @@ private:
     void createToolBar();
     void createStatusBar();
     void setupUI();
+    void createCentralWidget();
 
 private:
+    // Menus
     QMenu *fileMenu;
     QMenu *scanMenu;
     QMenu *helpMenu;
     QToolBar *mainToolBar;
+    
+    // Central widget components
+    QWidget *centralWidget;
+    QVBoxLayout *mainLayout;
+    QGridLayout *infoLayout;
+    
+    // Info labels
+    QLabel *ipLabel;
+    QLabel *macLabel;
+    QLabel *otherLabel1;
+    QLabel *otherLabel2;
+    
+    // Operation result
+    QTextEdit *operationResult;
+    
+    // Buttons
+    QPushButton *scanButton;
+    QPushButton *pingButton;
+    QPushButton *exportCsvButton;
+    QPushButton *exportJsonButton;
 };
 
 #endif // MAINWINDOW_H
